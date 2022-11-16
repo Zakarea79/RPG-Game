2ce7201f7d3a940e69ef81e6b8e27b6daad69127
Zakarea <Zakarea6688@gmail.com>
2022-11-15 21:06:41 -0800

    Update raycast.cs

--- Assets/Script/Camera/raycast.cs
+++ Assets/Script/Camera/raycast.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class raycast : MonoBehaviour
 {
 	[SerializeField] private Material GlassMaterial;
-	private Dictionary<GameObject , Material> objectRaymat = new Dictionary<GameObject , Material>();
-    private void OnTriggerEnter(Collider other) 
+	
+	private Dictionary<GameObject , List<Material>> objectRaymat = new Dictionary<GameObject , List<Material>>();
+	
+	private void OnTriggerEnter(Collider other)
 	{
-		MeshRenderer mes;
-		if(other.name != "Player" && other.TryGetComponent<MeshRenderer>(out mes))
+		MeshRenderer meshRenderer;
+		SkinnedMeshRenderer skinnedMeshRenderer;
+		var ListGlas = new List<Material>();
+
+		if(other.name != "Player" && other.TryGetComponent<MeshRenderer>(out meshRenderer))
 		{
-			objectRaymat.Add(other.gameObject , mes.material);
-			mes.material = GlassMaterial;
+			var list = new List<Material>();
+			list.AddRange(meshRenderer.materials);
+			objectRaymat.Add(other.gameObject , list);
+			
+			for (int i = 0; i < meshRenderer.materials.Length; i++) 
+			{
+				ListGlas.Add(GlassMaterial);
+			}
+			meshRenderer.materials = ListGlas.ToArray();
+		}
+		else if(other.name != "Player" && other.TryGetComponent<SkinnedMeshRenderer>(out skinnedMeshRenderer))
+		{
+			var list = new List<Material>();
+			list.AddRange(skinnedMeshRenderer.materials);
+			objectRaymat.Add(other.gameObject , list);
+			
+			for (int i = 0; i < skinnedMeshRenderer.materials.Length; i++) 
+			{
+				ListGlas.Add(GlassMaterial);
+			}
+			skinnedMeshRenderer.materials = ListGlas.ToArray();
 		}
 	}
 
@@ -20,8 +44,29 @@ public class raycast : MonoBehaviour
 	{
 		if(objectRaymat.ContainsKey(other.gameObject))
 		{
-			other.gameObject.GetComponent<MeshRenderer>().material = objectRaymat[other.gameObject];
-			objectRaymat.Remove(other.gameObject);
+			MeshRenderer meshRenderer;
+			SkinnedMeshRenderer skinnedMeshRenderer;
+			var ListGlas = new List<Material>();
+			
+			if(other.TryGetComponent<MeshRenderer>(out meshRenderer))
+			{
+				
+				for (int i = 0; i < meshRenderer.materials.Length; i++) 
+				{
+					ListGlas.Add(objectRaymat[other.gameObject][i]);
+				}
+				meshRenderer.materials = ListGlas.ToArray();
+				objectRaymat.Remove(other.gameObject);
+			}
+			else if(other.TryGetComponent<SkinnedMeshRenderer>(out skinnedMeshRenderer))
+			{
+				for (int i = 0; i < skinnedMeshRenderer.materials.Length; i++) 
+				{
+					ListGlas.Add(objectRaymat[other.gameObject][i]);
+				}
+				skinnedMeshRenderer.materials = ListGlas.ToArray();
+				objectRaymat.Remove(other.gameObject);
+			}
 		}
 	}
 }
